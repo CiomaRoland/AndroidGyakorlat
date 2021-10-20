7d3dd1714994dd6ec546a545032b442c136899fa
CiomaRoland <79277745+CiomaRoland@users.noreply.github.com>
2021-10-20 11:39:10 +0300

    lab5

--- QuizApp/app/src/main/java/com/example/quizapp/DisplayName.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/DisplayName.kt
@@ -2,13 +2,14 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.TextView
 
-
-
-class MainActivity : AppCompatActivity() {
-
+class DisplayName : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_display_name)
+
     }
 }
